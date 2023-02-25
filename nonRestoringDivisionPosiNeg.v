17da0f29de5a24747157f17ad5eeeeeedce0c3c7
William-Qu <789william@hotmail.ca>
2023-02-25 17:32:33 -0500

    NOT and Positive/Negative Division

--- nonRestoringDivisionPosiNeg.v
+++ nonRestoringDivisionPosiNeg.v
@@ -1,19 +1,31 @@
-module nonRestoringDivision(
+module nonRestoringDivisionPosiNeg(
 input [31:0] dividend, divisor,
 output [31:0] quotient, remainder
 );
 integer i;
 
-reg [31:0] divisorTwos, A, Q; //divisor is the same as M in the lecture slides, and divisorTwos is (-1 * M)
+reg [31:0] M, divisorTwos, A, Q; //divisorTwos is (-1 * M)
+wire [31:0] tempM;
 
 //Find the Two's Compliment of divisor
 twoCompliment two (divisor, divisorTwos);
 
+assign tempM = divisor[31:0];
+
 	always @ (*) begin
-		//Initialize Q
+		
+	
+		//Initialize Q and M depending on if the given divisor is positive or negative
 		for (i=0; i<32; i=i+1) 
 				begin
 					Q[i] <= dividend[i];
+					if (divisor[31] == 0) begin
+						M[i] <= divisor[i]; 
+					end
+					else if (divisor[31] == 1) begin
+						M[i] <= divisorTwos[i];
+						divisorTwos[i] <= tempM[i];						
+					end
 				end
 				
 		//Step 1 of the Non-Restoring Division Algorithm (Done 32 times)
@@ -35,6 +47,17 @@ twoCompliment two (divisor, divisorTwos);
 		
 		//If A < 0, add M to A to ensure a positive remainder
 		if (A[31] == 1) A = A + divisor;
+		
+		//Determine whether the outputs should be positive or negative (positives we don't do anythin 
+		//if (divisor[31] == 1 && dividend[31] == 1) ; else
+		if ((divisor[31] == 0 && dividend[31] == 1) || (divisor[31] == 1 && dividend[31] == 0))begin
+			for (i=0; i<32; i=i+1) 
+			begin
+				Q[i] <= !(Q[i]);
+			end
+			Q <= Q + 'b1;
+		end
+		//if (divisor[31] == 0 && dividend[31] == 0) ;
 	end
 	
 	//Set the outputs (quotient = Q and remainder = A)
