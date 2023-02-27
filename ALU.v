deccdfa0fe5c83854027b14208561d74b72f464b
William-Qu <789william@hotmail.ca>
2023-02-26 19:11:05 -0500

    Finished ALU and Control Unit

--- ALU.v
+++ ALU.v
@@ -1,24 +1,73 @@
 module ALU(
-input clk, clr, stop,
+input clk,
 input [31:0] regA, regB,
 input [4:0] OPCode, 
-output [31:0] regLO, regHI
+output [63:0] regZ
 );
+integer i;
 
+wire [31:0] regLOshl;
+wire [31:0] regLOshrl;
+wire [31:0] regLOshra;
+wire [31:0] regLOrotatel;
+wire [31:0] regLOrotater;
+wire [31:0] regLOlog_and;
+wire [31:0] regLOlog_or;
+wire [31:0] regLOnot;
+wire [31:0] regLOneg;
+wire [31:0] regLOadd, regHIadd;
+wire [31:0] regLOsub, regHIsub;
+wire [31:0] regLOmul, regHImul;
+wire [31:0] regLOdiv, regHIdiv;
+
+reg [31:0] regLO, regHI;
+
+//Generate the computational hardware, this will perform all operations, and then we will then later choose the one we want
 	generate
-		if (OPCode == 'b00001) left_shift SHL (clk, clr, regA regLO) ; else 						//SHL Module Name: 
-		if (OPCode == 'b00010) right_shift_combined SHRL (clk, clr, 'b0, regA, regLO); else 				//SHR logical Module Name:
-		if (OPCode == 'b00010) right_shift_combined SHRA (clk, clr, 'b1, regA regLO); else				//SHR arithmetic Module Name: 
-		if (OPCode == 'b00011) left_rotate rotateL ( clk, clr, regA, regLO); else 					//Rotate L Module Name: 
-		if (OPCode == 'b00100) right_rotate rotateR (clk, clr, regA, regLO); else 					//Rotate R Module Name: 
-		if (OPCode == 'b00101) logical_and log_and (regA, regB regLO); else 						//AND Module Name: 
-		if (OPCode == 'b00110) logical_or log_or (regA, regB, regLO); else 						//OR Module Name: 
-		if (OPCode == 'b00111) NOT Not (regA, regLO); else 								//NOT Module Name: NOT
-		if (OPCode == 'b01000) twoCompliment neg (regA, regLO); else 							//Neg Module Name: twoCompliment
-		if (OPCode == 'b01001) Hierarchical_CLA add('b0, regA, regB, regHI, regLO); else 				//Add Module Name: 
-		if (OPCode == 'b01010) Hierarchical_CLA subtract('b1, regA, regB, regHI, regLO) ; else 				//Subtract Module Name: 
-		if (OPCode == 'b01011) boothMultiplier mul (regA, regB, regHI, regLO); else 					//Multiply Module Name: boothMultiplier
-		if (OPCode == 'b01100) nonRestoringDivisionPosiNeg div (regA, regB, regLO, regHI);  				//Divide Module Name: nonRestoringDivisionPosiNeg
+		left_shift SHL (clk, clr, regA, regLOshl);  								//SHL Module Name: left_shift
+		right_shift_combined SHRL (clk, clr, 'b0, regA, regLOshrl);  		//SHR logical Module Name: right_shift_combined
+		right_shift_combined SHRA (clk, clr, 'b1, regA, regLOshra); 		//SHR arithmetic Module Name: right_shift_combined
+		left_rotate rotateL ( clk, clr, regA, regLOrotatel);  				//Rotate L Module Name: left_rotate
+		right_rotate rotateR (clk, clr, regA, regLOrotater);  				//Rotate R Module Name: left_rotate
+		logical_and log_and (regA, regB, regLOlog_and); 						//AND Module Name: logical_and
+		logical_or log_or (regA, regB, regLOlog_or);  							//OR Module Name: logical_or
+		NOT Not (regA, regLOnot);  													//NOT Module Name: NOT
+		twoCompliment neg (regA, regLOneg); 										//Neg Module Name: twoCompliment
+		Hierarchical_CLA add('b0, regA, regB, regHIadd, regLOadd);  		//Add Module Name: Hierarchical_CLA
+		Hierarchical_CLA sub('b1, regA, regB, regHIsub, regLOsub) ;  		//Subtract Module Name: Hierarchical_CLA
+		boothMultiplier mul (regA, regB, regHImul, regLOmul); 				//Multiply Module Name: boothMultiplier
+		nonRestoringDivisionPosiNeg div (regA, regB, regLOdiv, regHIdiv); //Divide Module Name: nonRestoringDivisionPosiNeg
 	endgenerate
+
+	always @ (*) begin
+		if (OPCode == 'b00001) regLO = regLOshl;	else							//SHL Module Name: left_shift
+		if (OPCode == 'b00010) regLO = regLOshrl; else							//SHR logical Module Name: right_shift_combined
+		if (OPCode == 'b00011) regLO = regLOshra; else							//SHR arithmetic Module Name: right_shift_combined
+		if (OPCode == 'b00100) regLO = regLOrotatel; else						//Rotate L Module Name: left_rotate
+		if (OPCode == 'b00101) regLO = regLOrotater; else						//Rotate R Module Name: left_rotate
+		if (OPCode == 'b00110) regLO = regLOlog_and; else						//AND Module Name: logical_and
+		if (OPCode == 'b00111) regLO = regLOlog_or; else						//OR Module Name: logical_or
+		if (OPCode == 'b01000) regLO = regLOnot; else							//NOT Module Name: NOT
+		if (OPCode == 'b01001) regLO = regLOneg; else							//Neg Module Name: twoCompliment
+		if (OPCode == 'b01010)begin													//Add Module Name: Hierarchical_CLA
+			regLO = regLOadd;	
+			regHI = regHIadd;
+		end else
+		if (OPCode == 'b01011)begin													//Subtract Module Name: Hierarchical_CLA
+			regLO = regLOsub;
+			regHI = regHIsub;
+		end else
+		if (OPCode == 'b01100)begin													//Multiply Module Name: boothMultiplier
+			regLO = regLOmul;	
+			regHI = regHImul;
+		end else
+		if (OPCode == 'b01101)begin													//Divide Module Name: nonRestoringDivisionPosiNeg
+			regLO = regLOdiv;	
+			regHI = regHIdiv;
+		end
+	end
+	
+	assign regZ[31:0]  = regLO[31:0];
+	assign regZ[63:32] = regHI[31:0];
 	
 endmodule
--- ControlUnit.v
+++ ControlUnit.v
@@ -1,109 +1,148 @@
 module ControlUnit(
 input clk, clr, wren, 
-input [31:0] IR,
-output 
-)
-//Built in registers to the ALU/ControlUnit
-reg [31:0] r0;
-reg [31:0] r1;
-reg [31:0] r2;
-reg [31:0] r3;
-reg [31:0] r4;
-reg [31:0] r5;
-reg [31:0] r6;
-reg [31:0] r7;
-reg [31:0] r8;
-reg [31:0] r9;
-reg [31:0] r10;
-reg [31:0] r11;
-reg [31:0] r12;
-reg [31:0] r13;
-reg [31:0] r14;
-reg [31:0] r15;
+input [31:0] IR
+);
+
+//Create the wires that will feed out of the ALU/Control Units General Registers
+wire [31:0] r0out;
+wire [31:0] r1out;
+wire [31:0] r2out;
+wire [31:0] r3out;
+wire [31:0] r4out;
+wire [31:0] r5out;
+wire [31:0] r6out;
+wire [31:0] r7out;
+wire [31:0] r8out;
+wire [31:0] r9out;
+wire [31:0] r10out;
+wire [31:0] r11out;
+wire [31:0] r12out;
+wire [31:0] r13out;
+wire [31:0] r14out;
+wire [31:0] r15out;
+
+//These registers will feed into the ALU/Control Units General Registers
+reg [31:0] r0in;  
+reg [31:0] r1in;  
+reg [31:0] r2in;  
+reg [31:0] r3in;  
+reg [31:0] r4in;  
+reg [31:0] r5in;  
+reg [31:0] r6in;  
+reg [31:0] r7in;  
+reg [31:0] r8in;  
+reg [31:0] r9in;  
+reg [31:0] r10in;
+reg [31:0] r11in;
+reg [31:0] r12in;
+reg [31:0] r13in;
+reg [31:0] r14in;
+reg [31:0] r15in;
+
+//Create the general 32-bit registers for use in the ALU/Control Unit from our code with input and output wires
+	generate 
+		reg32bit gr0  (r0out, r0in, clr, clk, wren);
+		reg32bit gr1  (r1out, r1in, clr, clk, wren);
+		reg32bit gr2  (r2out, r2in, clr, clk, wren);
+		reg32bit gr3  (r3out, r3in, clr, clk, wren);
+		reg32bit gr4  (r4out, r4in, clr, clk, wren);
+		reg32bit gr5  (r5out, r5in, clr, clk, wren);
+		reg32bit gr6  (r6out, r6in, clr, clk, wren);
+		reg32bit gr7  (r7out, r7in, clr, clk, wren);
+		reg32bit gr8  (r8out, r8in, clr, clk, wren);
+		reg32bit gr9  (r9out, r9in, clr, clk, wren);
+		reg32bit gr10 (r10out, r10in, clr, clk, wren);
+		reg32bit gr11 (r11out, r11in, clr, clk, wren);
+		reg32bit gr12 (r12out, r12in, clr, clk, wren);
+		reg32bit gr13 (r13out, r13in, clr, clk, wren);
+		reg32bit gr14 (r14out, r14in, clr, clk, wren);
+		reg32bit gr15 (r15out, r15in, clr, clk, wren);
+	endgenerate
 
 //IR Block Broken up
 wire [4:0] OPCode;
 wire [3:0] regASel, regBSel, destSel;
-wire [31:0] dest, regA, regB;
+reg [31:0] regA, regB;
 wire [63:0] regZ;
 
-//Break Up IR Block
+//Break Up IR Input
 assign OPCode = IR[31:27];
 assign destSel = IR[26:23];
 assign regASel = IR[22:19];
 assign regBSel = IR[18:15];
 
 //Temporary answers
-wire [31:0] regLO, regHI, tempDest;
+wire [31:0] regLO, regHI;
 
 //Using regASel and regBSel, get the contents of the correct registers into regA and regB
 	always @ (*) begin
 		//Put contents of the correct register into regA using regASel
-		if (regASel == 'b0000) regA = r0[31:0]; else
-		if (regASel == 'b0001) regA = r1[31:0]; else
-		if (regASel == 'b0010) regA = r2[31:0]; else
-		if (regASel == 'b0011) regA = r3[31:0]; else
-		if (regASel == 'b0100) regA = r4[31:0]; else
-		if (regASel == 'b0101) regA = r5[31:0]; else
-		if (regASel == 'b0110) regA = r6[31:0]; else
-		if (regASel == 'b0111) regA = r7[31:0]; else
-		if (regASel == 'b1000) regA = r8[31:0]; else
-		if (regASel == 'b1001) regA = r9[31:0]; else
-		if (regASel == 'b1010) regA = r10[31:0]; else
-		if (regASel == 'b1011) regA = r11[31:0]; else
-		if (regASel == 'b1100) regA = r12[31:0]; else
-		if (regASel == 'b1101) regA = r13[31:0]; else
-		if (regASel == 'b1110) regA = r14[31:0]; else
-		if (regASel == 'b1111) regA = r15[31:0];
+		if (regASel == 'b0000) regA = r0out[31:0]; else
+		if (regASel == 'b0001) regA = r1out[31:0]; else
+		if (regASel == 'b0010) regA = r2out[31:0]; else
+		if (regASel == 'b0011) regA = r3out[31:0]; else
+		if (regASel == 'b0100) regA = r4out[31:0]; else
+		if (regASel == 'b0101) regA = r5out[31:0]; else
+		if (regASel == 'b0110) regA = r6out[31:0]; else
+		if (regASel == 'b0111) regA = r7out[31:0]; else
+		if (regASel == 'b1000) regA = r8out[31:0]; else
+		if (regASel == 'b1001) regA = r9out[31:0]; else
+		if (regASel == 'b1010) regA = r10out[31:0]; else
+		if (regASel == 'b1011) regA = r11out[31:0]; else
+		if (regASel == 'b1100) regA = r12out[31:0]; else
+		if (regASel == 'b1101) regA = r13out[31:0]; else
+		if (regASel == 'b1110) regA = r14out[31:0]; else
+		if (regASel == 'b1111) regA = r15out[31:0];
 		
 		//Put contents of the correct register into regB using regBSel
-		if (regBSel == 'b0000) regB = r0[31:0]; else
-		if (regBSel == 'b0001) regB = r1[31:0]; else
-		if (regBSel == 'b0010) regB = r2[31:0]; else
-		if (regBSel == 'b0011) regB = r3[31:0]; else
-		if (regBSel == 'b0100) regB = r4[31:0]; else
-		if (regBSel == 'b0101) regB = r5[31:0]; else
-		if (regBSel == 'b0110) regB = r6[31:0]; else
-		if (regBSel == 'b0111) regB = r7[31:0]; else
-		if (regBSel == 'b1000) regB = r8[31:0]; else
-		if (regBSel == 'b1001) regB = r9[31:0]; else
-		if (regBSel == 'b1010) regB = r10[31:0]; else
-		if (regBSel == 'b1011) regB = r11[31:0]; else
-		if (regBSel == 'b1100) regB = r12[31:0]; else
-		if (regBSel == 'b1101) regB = r13[31:0]; else
-		if (regBSel == 'b1110) regB = r14[31:0]; else
-		if (regBSel == 'b1111) regB = r15[31:0];
+		if (regBSel == 'b0000) regB = r0out[31:0]; else
+		if (regBSel == 'b0001) regB = r1out[31:0]; else
+		if (regBSel == 'b0010) regB = r2out[31:0]; else
+		if (regBSel == 'b0011) regB = r3out[31:0]; else
+		if (regBSel == 'b0100) regB = r4out[31:0]; else
+		if (regBSel == 'b0101) regB = r5out[31:0]; else
+		if (regBSel == 'b0110) regB = r6out[31:0]; else
+		if (regBSel == 'b0111) regB = r7out[31:0]; else
+		if (regBSel == 'b1000) regB = r8out[31:0]; else
+		if (regBSel == 'b1001) regB = r9out[31:0]; else
+		if (regBSel == 'b1010) regB = r10out[31:0]; else
+		if (regBSel == 'b1011) regB = r11out[31:0]; else
+		if (regBSel == 'b1100) regB = r12out[31:0]; else
+		if (regBSel == 'b1101) regB = r13out[31:0]; else
+		if (regBSel == 'b1110) regB = r14out[31:0]; else
+		if (regBSel == 'b1111) regB = r15out[31:0];
 	end
 
 //Do the computation with an ALU unit and get the output in the Z register
 	generate
-		ALU compute (clk, regA, regB, OPCode, regZ)
+		ALU compute (clk, regA, regB, OPCode, regZ);
 	endgenerate
 	
-//Set the HI and LO registers from the values attained in the Z register
+//Split the Z register into regLO and regHI
 assign regLO = regZ[31:0];
 assign regHI = regZ[63:32];
 
-	//This generate statement will be to determine which register to put the datas output into depending on destSel and if its not mult or div
-	generate
-		if (OPCode != 'b && OPCode != 'b) begin //If not multiplication or division, put regLO in proper register
-			if (destSel == 'b0000) reg32bit tr0  (r0, regLO, clr, clk, wren); else
-			if (destSel == 'b0001) reg32bit tr1  (r1, regLO, clr, clk, wren); else
-			if (destSel == 'b0010) reg32bit tr2  (r2, regLO, clr, clk, wren); else
-			if (destSel == 'b0011) reg32bit tr3  (r3, regLO, clr, clk, wren); else
-			if (destSel == 'b0100) reg32bit tr4  (r4, regLO, clr, clk, wren); else
-			if (destSel == 'b0101) reg32bit tr5  (r5, regLO, clr, clk, wren); else
-			if (destSel == 'b0110) reg32bit tr6  (r6, regLO, clr, clk, wren); else
-			if (destSel == 'b0111) reg32bit tr7  (r7, regLO, clr, clk, wren); else
-			if (destSel == 'b1000) reg32bit tr8  (r8, regLO, clr, clk, wren); else
-			if (destSel == 'b1001) reg32bit tr9  (r9, regLO, clr, clk, wren); else
-			if (destSel == 'b1010) reg32bit tr10 (r10, regLO, clr, clk, wren); else
-			if (destSel == 'b1011) reg32bit tr11 (r11, regLO, clr, clk, wren); else
-			if (destSel == 'b1100) reg32bit tr12 (r12, regLO, clr, clk, wren); else
-			if (destSel == 'b1101) reg32bit tr13 (r13, regLO, clr, clk, wren); else
-			if (destSel == 'b1110) reg32bit tr14 (r14, regLO, clr, clk, wren); else
-			if (destSel == 'b1111) reg32bit tr15 (r15, regLO, clr, clk, wren);
+//This always statement will be to determine which register to put the operations output into depending on destSel and if its not mult or div (Since these don't have a destination slot yet)
+//NOTE: There may be overflow from addition, this will just be left in regHI
+	always @ (*) begin
+		if (OPCode != 'b01100 && OPCode != 'b01101) begin //If not mult, or div, put regLO in proper register
+			//if (destSel == 'b0000) r0in <= regLO; else //r0 should always remain 0
+			if (destSel == 'b0001) r1in <= regLO; else
+			if (destSel == 'b0010) r2in <= regLO; else
+			if (destSel == 'b0011) r3in <= regLO; else
+			if (destSel == 'b0100) r4in <= regLO; else
+			if (destSel == 'b0101) r5in <= regLO; else
+			if (destSel == 'b0110) r6in <= regLO; else
+			if (destSel == 'b0111) r7in <= regLO; else
+			if (destSel == 'b1000) r8in <= regLO; else
+			if (destSel == 'b1001) r9in <= regLO; else
+			if (destSel == 'b1010) r10in <= regLO; else
+			if (destSel == 'b1011) r11in <= regLO; else
+			if (destSel == 'b1100) r12in <= regLO; else
+			if (destSel == 'b1101) r13in <= regLO; else
+			if (destSel == 'b1110) r14in <= regLO; else
+			if (destSel == 'b1111) r15in <= regLO;
 		end
-	endgenerate
+	end
 
 endmodule
